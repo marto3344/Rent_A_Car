c922deab47d321d52dcd7f3494ff9618bcfaedfe
Martin  Stoyanov <martotbb.2556@gmail.com>
2023-04-08 11:57:23 +0300

    Car details and car list view

--- Rent_A_Car_Web/Controllers/CarsController.cs
+++ Rent_A_Car_Web/Controllers/CarsController.cs
@@ -20,7 +20,23 @@ namespace Rent_A_Car_Web.Views.Cars
         {
             return View(await _context.Cars.ToListAsync());
         }
-        
-       
+        public async Task<IActionResult> CarDetails(int? id)
+        {
+            if (id == null || _context.Cars == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _context.Cars
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
+
     }
 }
