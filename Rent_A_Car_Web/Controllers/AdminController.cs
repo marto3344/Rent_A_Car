3d15b2bead2dcc86d9340c2f91972e7aaa6e5b0f
Martin  Stoyanov <martotbb.2556@gmail.com>
2023-04-23 09:06:30 +0300

    validation for car creation

--- Rent_A_Car_Web/Controllers/AdminController.cs
+++ Rent_A_Car_Web/Controllers/AdminController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Rent_A_Car_Web.Data;
-
+using System.Globalization;
 namespace Rent_A_Car_Web.Controllers
 {
     public class AdminController : Controller
@@ -29,13 +29,25 @@ namespace Rent_A_Car_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Brand,Model,ImageUrl,Seat,Year,Description,Price")] Car car)//Metod za dobacqne na kola
         {
+            if (car.Seat < 0 ||car.Seat>7)//Custom validaciq
+            {
+                ModelState.AddModelError(nameof(car.Seat), $"Seats must be between 1 and 7");
+            }
+            if(car.Year<1980 ||car.Year >DateTime.Today.Year)
+            {
+                ModelState.AddModelError(nameof(car.Year), $"Year must be between 1980 and {DateTime.Today.Year}");
+            }
+            if(car.Price<=0)
+            {
+                ModelState.AddModelError(nameof(car.Price), $"Car price must be higher than 0");
+            }
             if (ModelState.IsValid)
             {
                 _context.Add(car);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(car);
+            return View("CreateCar");
         }
 
         public async Task<IActionResult> EditCar(int? id)//Vrushta view kogato e izbrana suotvetnata kola
@@ -54,11 +66,34 @@ namespace Rent_A_Car_Web.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditCar(int id, [Bind("Id,Brand,Model,ImageUrl,Seat,Year,Description,Price")] Car car)//Logika za redaktirane kola
+        public async Task<IActionResult> EditCar(int id, [Bind("Id,Brand,Model,ImageUrl,Seat,Year,Description,Price,RentStart,RentEnd")] Car car)//Logika za redaktirane kola
         {
             if (id != car.Id)
             {
                 return NotFound();
+            }
+
+            if (car.Seat < 0 || car.Seat > 7)//Custom validaciq
+            {
+                ModelState.AddModelError(nameof(car.Seat), $"Seats must be between 1 and 7");
+            }
+            if (car.Year < 1980 || car.Year > DateTime.Today.Year)
+            {
+                ModelState.AddModelError(nameof(car.Year), $"Year must be between 1980 and {DateTime.Today.Year}");
+            }
+            if (car.Price <= 0)
+            {
+                ModelState.AddModelError(nameof(car.Price), $"Car price must be higher than 0");
+            }
+            if (car.RentEnd < car.RentStart)
+            {
+                ModelState.AddModelError(nameof(car.RentStart),
+                                         $"Rent end date can't be before rent start date!Please correct the format!");
+            }
+            if (car.RentStart.Date < DateTime.Today)
+            {
+                ModelState.AddModelError(nameof(car.RentStart),
+                                         $"Rent cant start at {car.RentStart.ToString("dd/MM/yyyy")}. Today is {DateTime.Today.ToString("dd/MM/yyyy")}. Please enter valid date!");
             }
 
             if (ModelState.IsValid)
@@ -138,6 +173,14 @@ namespace Rent_A_Car_Web.Controllers
         {
             return _context.Cars.Any(c => c.Id == id);
         }
-       
+        private void CarValidation (Car car)
+        {
+            if (car.Seat < 0)
+            {
+                ModelState.AddModelError(nameof(Car.Seat), $"Seats must be positive digit");
+
+            }
+
+        }
     }
 }
--- Rent_A_Car_Web/Controllers/CarsController.cs
+++ Rent_A_Car_Web/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -17,7 +18,7 @@ namespace Rent_A_Car_Web.Views.Cars
         private readonly UserManager<User> _userManager;
         public DateTime RentStart { get; set; }
         public DateTime RentEnd { get; set; }
-        public CarsController(ApplicationDbContext context, UserManager<User>userManager)
+        public CarsController(ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
             _userManager = userManager;
@@ -44,6 +45,7 @@ namespace Rent_A_Car_Web.Views.Cars
         }
         public async Task<IActionResult> RentCar(int? id)//Vrushta view kogato e izbrana suotvetnata kola
         {
+
             if (id == null || _context.Cars == null)
             {
                 return NotFound();
@@ -58,7 +60,7 @@ namespace Rent_A_Car_Web.Views.Cars
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> RentCar(int id, [Bind("Id,Brand,Model,ImageUrl,Seat,Year,Description,Price")] Car car )//Logika za naemane na kola
+        public async Task<IActionResult> RentCar(int id, [Bind("Id,Brand,Model,ImageUrl,Seat,Year,Description,Price")] Car? car)//Logika za naemane na kola
         {
             var user = _userManager.FindByEmailAsync(User.Identity?.Name).Result;
             car = await _context.Cars
@@ -67,6 +69,20 @@ namespace Rent_A_Car_Web.Views.Cars
             {
                 return NotFound();
             }
+            if (this.RentStart.Date < DateTime.Today)
+            {
+                ModelState.AddModelError(nameof(car.RentStart),
+                                         $"Rent cant start at {this.RentStart.ToString("dd/MM/yyyy")}. Today is {DateTime.Today.ToString("dd/MM/yyyy")}. Please enter valid date!");
+                return View("CarDetails", car);
+            }
+            if (this.RentEnd < this.RentStart)
+            {
+                ModelState.AddModelError(nameof(car.RentStart),
+                                         $"Rent end date can't be before rent start date!Please correct the format!");
+
+                return View("CarDetails", car);
+            }
+
             car.Availabilyty = false;
             car.RentStart = this.RentStart;
             car.RentEnd = this.RentEnd;
@@ -74,7 +90,7 @@ namespace Rent_A_Car_Web.Views.Cars
             _context.Update(car);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index", "Profile");
-           
+
 
         }
         private bool CarExists(int id)//Proverka dali sushtestvuva tazi kola v sistemat
